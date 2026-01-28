0a922059a0feb7ca95c8492bbd0998b09ba8ce91
Benoit Vasseur <bvasseur@coreoz.com>
2026-01-28 15:53:42 +0100

    init project

---
+++ background.js
@@ -0,0 +1,20 @@
+// Service worker pour l'extension Riven Market
+
+// Ouvrir le sidepanel quand on clique sur l'icône de l'extension
+chrome.action.onClicked.addListener((tab) => {
+  chrome.sidePanel.open({ windowId: tab.windowId });
+});
+
+// Gestion de l'installation de l'extension
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Riven Market extension installée avec succès');
+});
+
+// Écouter les messages du sidepanel
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'getData') {
+    // Logique pour récupérer des données
+    sendResponse({ success: true, data: 'Données du marché Riven' });
+  }
+  return true;
+});
